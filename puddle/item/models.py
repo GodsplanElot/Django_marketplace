efe21756395c5024224f151a374d9642d53e14bb
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-18 17:11:18 +0100

    created the models for the items

--- puddle/item/models.py
+++ puddle/item/models.py
@@ -1,6 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
+
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
+class item(models.Model):
+    Category = models.ForeignKey(Category, related_name='item', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField()
+    image = models.ImageField(upload_to='item_images', blank=True, null=True)
+    is_sold = models.BooleanField(default=False)
+    created_by = models.ForeignKey(User, related_name='items', on_delete=models.CASCADE)
+    created_at = models.DateField(auto_now_add=True)
