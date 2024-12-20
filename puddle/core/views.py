6d85bfa62f4a6a69da6c59712bb3c93c8b068a2d
GodsplanELot <elotgodsplan2@gmail.com>
2024-12-20 12:43:30 +0100

    dev pump

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from item.models import Category, item
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'core/index.html')
+    items = item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'core/index.html', {
+        'categories': categories,
+        'items': items,
+        })
+
 
 def contact(request):
     return render(request, 'core/contact.html')
